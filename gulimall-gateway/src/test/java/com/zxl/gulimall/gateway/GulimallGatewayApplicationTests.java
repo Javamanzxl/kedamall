e1602e66150e9278e33e0564e34d1c3bd92797bc
zxl <1050295916@qq.com>
2024-10-22 21:02:37 +0800

    补充

---
+++ gulimall-gateway/src/test/java/com/zxl/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.gulimall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-third-party/src/test/java/com/zxl/gulimall/thirdParty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.gulimall.thirdParty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
